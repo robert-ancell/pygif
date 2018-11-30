f8b5d6d1be42a67c63401e1d038242a8f6394be5
Robert Ancell <robert.ancell@canonical.com>
2018-11-30 14:05:04 +1300

    Add license header

--- gif.py
+++ gif.py
@@ -1,4 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+
+# Copyright 2018 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3, as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __version__  = '0.1'
 __all__      = [ 'AnimationExtension',
