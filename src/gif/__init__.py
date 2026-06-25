ef7d18e29209b93f12aaeb1928005d766df44c8d
Robert Ancell <robert.ancell@gmail.com>
2026-06-25 22:18:09 +1200

    Add back __version__

--- src/gif/__init__.py
+++ src/gif/__init__.py
@@ -20,7 +20,10 @@ from gif.lzw import LZWDecoder, LZWEncoder
 from gif.reader import Reader
 from gif.writer import Writer
 
+from .__about__ import __version__
+
 __all__ = [
+    "__version__",
     "AnimationExtension",
     "ApplicationExtension",
     "Block",
